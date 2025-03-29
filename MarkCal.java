270307fdce543db60aab9da63abdce6463e11a12
Your Name <rakeshpatro7653@gmail.com>
2025-03-30 01:49:05 +0530

    MarkCal.java

---
+++ MarkCal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MarkCal {
+    public static void main(String[] args) {
+        System.out.println("Enter student marks....");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter math marks: ");
+        float math=sc.nextFloat();
+        System.out.print("Enter phy marks: ");
+        float phy = sc.nextFloat();
+        System.out.print("Enter chem marks: ");
+        float chem= sc.nextFloat();
+        System.out.print("Enter bio marks: ");
+        float Bio=sc.nextFloat();
+        System.out.print("Enter IT marks: ");
+        float IT=sc.nextFloat();
+        System.out.println(" Total marks: ");
+        int totalmarks=sc.nextInt();
+        float sum = math + phy + chem + Bio + IT;
+        System.out.println("Secure marks: " +sum);
+        float percentage = (sum/totalmarks) *100;
+        System.out.print("Total percentage : "+percentage);
+
+        }
+    }
